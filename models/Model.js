8b64e43455fe04d2eaa96c1ef742925ab67b0fb8
penn <penn@JindeMacBook-Pro.local>
2018-03-23 01:12:44 +0800

    tmp save

--- models/Model.js
+++ models/Model.js
@@ -46,17 +46,26 @@ export const sequelize = new Sequelize('cltp', 'root', 'tcltcl', {
   operatorsAliases,
 });
 
-const ADMIN = 1;
-const PPJL = 2;
-const KFJL = 3;
-const GZ = 4;
-const GTBA = 5;
-const GYSGLY = 6;
-const AZGGLY = 7;
-const ZHY = 8;
-const AZG = 9;
+const JS = {
+  ADMIN: '系统管理员',
+  PPJL: '品牌经理',
+  KFJL: '客服经理',
+  GZ: '柜长',
+  GTBA: '柜台BA',
+  GYSGLY: '供应商管理员',
+  AZGGLY: '安装工管理员',
+  ZHY: '装货员',
+  AZG: '安装工',
+};
 
-const getBasicTable = (str, editRole = [ADMIN, PPJL]) =>
+const QY = {
+  EAST: '东区',
+  SOUTH: '南区',
+  WEST: '西区',
+  NORTH: '北区',
+};
+
+const getBasicTable = (str, editRole = [JS.ADMIN, JS.PPJL]) =>
   sequelize.define(
     str,
     {
@@ -85,130 +94,113 @@ export const User = sequelize.define(
   'User',
   {
     id: {
-      autoIncrement: true,
+      type: Sequelize.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
+    },
+    JS: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        enumCheck(val) {
+          if (!Object.values(JS).includes(val)) {
+            throw new Error('非法角色名称!');
+          }
+        },
+      },
+    },
+    PPId: {
       type: Sequelize.INTEGER,
     },
-
-    email: {
-      type: Sequelize.TEXT,
+    QY: {
+      type: Sequelize.STRING,
+      validate: {
+        enumCheck(val) {
+          if (!Object.values(QY).includes(val)) {
+            throw new Error('非法区域名称!');
+          }
+        },
+      },
     },
-
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
     },
+    name: {
+      type: Sequelize.STRING,
+    },
+    phone: {
+      type: Sequelize.STRING,
+    },
+    mail: {
+      type: Sequelize.STRING,
+    },
+    note: {
+      type: Sequelize.STRING,
+    },
+    GYSId: {
+      type: Sequelize.INTEGER,
+    },
+    AZGSId: {
+      type: Sequelize.INTEGER,
+    },
   },
   {
     freezeTableName: true,
+    paranoid: true,
+    version: true,
+    validate: {
+      check1() {
+        ppLog('1');
+        // throw new Error('Require either both latitude and longitude or neither1');
+      },
+      check2() {
+        ppLog('2');
+        // throw new Error('Require either both latitude and longitude or neither2');
+      },
+    },
   },
 );
 
-// 角色
-export const JS = getBasicTable('JS');
-
-User.belongsTo(JS, { as: 'JS' });
-
 // 品牌
 export const PP = getBasicTable('PP');
-User.belongsToMany(PP, { through: 'User_PP' });
-PP.belongsToMany(User, { through: 'User_PP' });
+User.belongsTo(PP, { foreignKey: 'PPId' });
+
+User.likeSearch = () => ['JS', 'PPId', 'QY', 'username', 'GYSId', 'AZGSId'];
 
 const initData = (str, num = 200) =>
   Array(num)
     .fill(0)
     .map((_, i) => ({ name: `${str}_${i}` }));
 
-const addUserPP = async (user) => {
-  const userId = user.id;
-  const ppLength = await PP.count();
-  const count = 5;
-  const ppId = Array(count)
-    .fill(0)
-    .map((_, i) => (userId + i) % ppLength + 1);
-
-  const pps = await PP.findAll({
-    where: {
-      id: {
-        [Op.in]: ppId,
-      },
-    },
-  });
-
-  user.addPPs(pps);
-};
-
 const createUser = async (js, num) => {
+  const ppLength = await PP.count();
   for (let i = 1; i <= num; i++) {
+    const PPId = i % ppLength;
     const u = await User.create({
-      email: `${js}_${i}@1.com`,
+      username: `u${i}`,
+      mail: `${js}_${i}@1.com`,
       password: bCrypt.hashSync('1', 8),
-      JSId: js,
+      JS: JS.PPJL,
+      PPId: PPId !== 0 ? PPId : ppLength,
     });
-    await addUserPP(u);
   }
 };
 
 export const init = async () => {
   try {
+    // Drop all tables
+    await sequelize.drop();
     await sequelize.sync({ force: true });
-
-    await JS.bulkCreate([
-      {
-        id: ADMIN,
-        name: '系统管理员',
-      },
-      {
-        id: PPJL,
-        name: '品牌经理',
-      },
-      {
-        id: KFJL,
-        name: '客服经理',
-      },
-      {
-        id: GZ,
-        name: '柜长',
-      },
-      {
-        id: GTBA,
-        name: '柜台BA',
-      },
-      {
-        id: GYSGLY,
-        name: '供应商管理员',
-      },
-      {
-        id: AZGGLY,
-        name: '安装工管理员',
-      },
-      {
-        id: ZHY,
-        name: '装货员',
-      },
-      {
-        id: AZG,
-        name: '安装工',
-      },
-    ]);
 
     await PP.bulkCreate(initData('品牌'));
 
-    await createUser(1, 10);
-
-    // const r = await User.findAll({
-    //   attributes: ['email'],
-    //   include: [
-    //     {
-    //       model: JS,
-    //       as: 'JS',
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
-    const r = await sequelize.query('SELECT * FROM `USER` JOIN `JS` ON `USER`.`JSId` = `JS`.`ID`', { type: sequelize.QueryTypes.SELECT });
-    ppLog(r);
-    // ppLog(r.map(item => item.toJSON()));
+    await createUser(1, 20);
   } catch (err) {
     ppLog('init err:', err);
   }
