cd7f3a944c9134237ac0659f8646a0b3bc22fff0
penn <penn@JindeMacBook-Pro.local>
2018-05-08 12:31:17 +0800

    tmp save

--- routes/business_apis/AnZhuangFanKuiBHDPZhuangTai.js
+++ routes/business_apis/AnZhuangFanKuiBHDPZhuangTai.js
@@ -3,22 +3,22 @@ import BusinessApiBase from '../BusinessApiBase';
 import * as DBTables from '../../models/Model';
 import * as ppUtils from './ppUtils';
 
-export default class AnZhuangFanKuiBHWLZhuangTai extends BusinessApiBase {
+export default class AnZhuangFanKuiBHDPZhuangTai extends BusinessApiBase {
   static getAllowAccessJSs() {
     return [DBTables.JS.GTBA, DBTables.JS.AZG];
   }
 
   static validateParamsFormat(req, res, next) {
-    // WYWLPayloads:
+    // WYDPPayloads:
     // [{
     // id: 1,
     // AZFKType: '安装成功',
     // imageUrl: 'xxxx'
     // }]
-    const { WYWLPayloads } = req.body;
+    const { WYDPPayloads } = req.body;
 
     // 检查AZFKType
-    const errorAZFKType = WYWLPayloads.filter(item => !Object.values(DBTables.AZFKType).includes(item.AZFKType));
+    const errorAZFKType = WYDPPayloads.filter(item => !Object.values(DBTables.AZFKType).includes(item.AZFKType));
     if (errorAZFKType.length > 0) {
       throw new Error(`${JSON.stringify(errorAZFKType)}中反馈状态有误!`);
     }
@@ -26,48 +26,48 @@ export default class AnZhuangFanKuiBHWLZhuangTai extends BusinessApiBase {
   }
 
   static async mainProcess(req, res, next, user, transaction) {
-    // WYWLPayloads:
+    // WYDPPayloads:
     // [{
     // id: 1,
     // AZFKType: '安装成功',
     // imageUrl: 'xxxx'
     // }]
-    const { WYWLPayloads } = req.body;
+    const { WYDPPayloads } = req.body;
 
     // 检查相关记录是否属于用户操作范围, 记录状态是否是可操作状态
 
     // 检查ids的存在
-    const ids = WYWLPayloads.map(item => item.id);
-    const tmpWYWLs = await ppUtils.checkIdsExistanceAndGetRecords(
-      'WYWL',
+    const ids = WYDPPayloads.map(item => item.id);
+    const tmpWYDPs = await ppUtils.checkIdsExistanceAndGetRecords(
+      'WYDP',
       ids,
       transaction,
     );
     // end 检查ids的存在
 
     // 检查是否属于当前用户的任务范围
-    for (const item of tmpWYWLs) {
-      if (!item.WLBHId) {
-        throw new Error(`${item}不属于物料补货!`);
+    for (const item of tmpWYDPs) {
+      if (!item.DPBHId) {
+        throw new Error(`${item}不属于灯片补货!`);
       }
-      const tmpWLBH = await item.getWLBH({ transaction });
-      await user.checkWLBHId(tmpWLBH.id, transaction);
+      const tmpDPBH = await item.getDPBH({ transaction });
+      await user.checkDPBHId(tmpDPBH.id, transaction);
     }
     // end 检查是否属于当前用户的任务范围
 
     // end 检查相关记录是否属于用户操作范围, 记录状态是否是可操作状态
 
     // 反馈
-    for (const item of WYWLPayloads) {
-      const tmpWYWL = tmpWYWLs.find(WYWL => WYWL.id === item.id);
+    for (const item of WYDPPayloads) {
+      const tmpWYDP = tmpWYDPs.find(WYDP => WYDP.id === item.id);
       if (item.AZFKType === DBTables.AZFKType.AZCG) {
         // 成功反馈
-        if (tmpWYWL.status !== DBTables.WYWLStatus.SH) {
-          throw new Error(`${tmpWYWL}状态不是${DBTables.WYWLStatus.SH}, 不能递交成功反馈`);
+        if (tmpWYDP.status !== DBTables.WYDPStatus.SH) {
+          throw new Error(`${tmpWYDP}状态不是${DBTables.WYDPStatus.SH}, 不能递交成功反馈`);
         }
-        await ppUtils.changeWYWLsStatus({
+        await ppUtils.changeWYDPsStatus({
           ids: [item.id],
-          status: DBTables.WYWLStatus.FK,
+          status: DBTables.WYDPStatus.FK,
           user,
           transaction,
           AZFKType: DBTables.AZFKType.AZCG,
@@ -75,19 +75,19 @@ export default class AnZhuangFanKuiBHWLZhuangTai extends BusinessApiBase {
         });
       } else {
         // 失败反馈
-        await ppUtils.changeWYWLsStatus({
+        await ppUtils.changeWYDPsStatus({
           ids: [item.id],
-          status: DBTables.WYWLStatus.FK,
+          status: DBTables.WYDPStatus.FK,
           user,
           transaction,
           AZFKType: item.AZFKType,
           imageUrl: item.imageUrl,
         });
       }
-      const tmpWLBH = await tmpWYWL.getWLBH({ transaction });
-      await tmpWLBH.update(
+      const tmpDPBH = await tmpWYDP.getDPBH({ transaction });
+      await tmpDPBH.update(
         {
-          status: DBTables.WLBHStatus.WC,
+          status: DBTables.DPBHStatus.WC,
         },
         {
           transaction,
