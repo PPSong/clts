91d38fbbb229c6793a485cdafaf6143b7753e9e0
jayliang701 <156070304@qq.com>
2018-05-19 17:09:28 +0800

    add new API and fix bug

---
+++ routes/business_apis/CreateAZG.js
@@ -0,0 +1,35 @@
+import bCrypt from 'bcryptjs';
+import BusinessApiBase from '../BusinessApiBase';
+import * as DBTables from '../../models/Model';
+
+export default class CreateAZG extends BusinessApiBase {
+  static getAllowAccessJSs() {
+    return [DBTables.JS.AZGSGLY];
+  }
+
+  static async mainProcess(req, res, next, user, transaction) {
+    const { name, username, password } = req.body;
+
+    let azgs = await user.getGLYAZGSs();
+    azgs = (azgs || [])[0];
+
+    if (!azgs) {
+      throw new Error('无此权限!');
+    }
+
+    const tmpAZGUser = await DBTables.User.create(
+      {
+        name,
+        username,
+        password: bCrypt.hashSync(password, 8),
+        JS: DBTables.JS.AZG,
+      },
+      { transaction }
+    );
+
+    const existedAZGs = await azgs.getAZGs();
+    existedAZGs.push(tmpAZGUser);
+
+    await azgs.setAZGs(existedAZGs, { transaction });
+  }
+}
