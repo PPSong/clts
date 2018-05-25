5e974351af70602a99d087814ceeb185582d78f4
jayliang701 <156070304@qq.com>
2018-05-25 08:38:44 +0800

    update menu

--- config/menus.js
+++ config/menus.js
@@ -1,15 +1,15 @@
 
 export default {
   "系统管理员":[
-    { name: '首页', key: '/dashboard' },
-    { name: '我的账户', key: '/me' },
-    { name: '账户管理', key: '/user' },
-    { name: '品牌管理', key: '/pp' },
-    { name: '柜台管理', key: '/gt' },
-    { name: '供应商管理', key: '/gys' },
-    { name: '安装公司管理', key: '/azgs' },
+    { name: '首页', icon:'Home', key: '/dashboard' },
+    { name: '我的', icon:'PlayerSettings', key: '/me' },
+    { name: '账户', key: '/user' },
+    { name: '品牌', key: '/pp' },
+    { name: '柜台', key: '/gt' },
+    { name: '供应商', key: '/gys' },
+    { name: '安装公司', html:'安装<br>公司', key: '/azgs' },
     {
-      name: '货品管理',
+      name: '货品',
       key: '#items',
       links:[
         { name: '灯位', key: '/dw' },
@@ -19,14 +19,14 @@ export default {
     },
   ],
   "品牌经理":[
-    { name: '首页', key: '/dashboard' },
-    { name: '我的账户', key: '/me' },
-    { name: '账户管理', key: '/user' },
-    { name: '柜台管理', key: '/gt' },
-    { name: '供应商查询', key: '/gys' },
-    { name: '安装公司查询', key: '/azgs' },
+    { name: '首页', icon:'Home', key: '/dashboard' },
+    { name: '我的', icon:'PlayerSettings', key: '/me' },
+    { name: '账户', key: '/user' },
+    { name: '柜台', key: '/gt' },
+    { name: '供应商', key: '/gys' },
+    { name: '安装公司', html:'安装<br>公司', key: '/azgs' },
     {
-      name: '货品管理',
+      name: '货品',
       key: '#items',
       links:[
         { name: '灯位', key: '/dw' },
@@ -36,14 +36,15 @@ export default {
     },
   ],
   "客服经理":[
-    { name: '首页', key: '/dashboard' },
-    { name: '我的账户', key: '/me' },
-    { name: '账户管理', key: '/user' },
-    { name: '柜台管理', key: '/gt' },
-    { name: '供应商查询', key: '/gys' },
-    { name: '安装公司查询', key: '/azgs' },
+    { name: '首页', icon:'Home', key: '/dashboard' },
+    { name: '我的', icon:'PlayerSettings', key: '/me' },
+    { name: '账户', icon:'Family', key: '/user' },
+    { name: '柜台', icon:'Archive', key: '/gt' },
+    { name: '供应商', icon:'SplitObject', key: '/gys' },
+    { name: '安装公司', icon:'Repair', html:'安装<br>公司', key: '/azgs' },
     {
-      name: '货品管理',
+      name: '货品',
+      icon:'Product', 
       key: '#items',
       links:[
         { name: '灯位', key: '/dw' },
@@ -53,13 +54,13 @@ export default {
     },
   ],
   "供应商管理员":[
-    { name: '首页', key: '/dashboard' },
-    { name: '我的账户', key: '/me' },
-    { name: '账户管理', key: '/user' }
+    { name: '首页', icon:'Home', key: '/dashboard' },
+    { name: '我的', icon:'PlayerSettings', key: '/me' },
+    { name: '账户', key: '/user' }
   ],
   "安装公司管理员":[
-    { name: '首页', key: '/dashboard' },
-    { name: '我的账户', key: '/me' },
-    { name: '账户管理', key: '/user' }
+    { name: '首页', icon:'Home', key: '/dashboard' },
+    { name: '我的', icon:'PlayerSettings', key: '/me' },
+    { name: '账户', key: '/user' }
   ]
 };
