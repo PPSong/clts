097b6cd7fb7a78aaa9e01b51778ed8f196313ebe
jayliang701 <156070304@qq.com>
2018-06-04 20:16:42 +0800

    add new api

--- routes/business_apis/SetWLBHs0YJAZDate.js
+++ routes/business_apis/SetWLBHs0YJAZDate.js
@@ -1,14 +1,25 @@
 import _ from 'lodash';
+import moment from 'moment';
 import BusinessApiBase from '../BusinessApiBase';
 import * as DBTables from '../../models/Model';
 
-export default class SetWLBHs0AZG extends BusinessApiBase {
+export default class SetWLBHs0YJAZDate extends BusinessApiBase {
   static getAllowAccessJSs() {
     return [DBTables.JS.AZGSGLY];
   }
 
+  static validateParamsFormat(req, res, next) {
+    const { ids, YJAZDate } = req.body;
+
+    // 检查时间格式
+    if (!moment(YJAZDate, 'YYYY-MM-DD').isValid()) {
+      throw new Error(`${YJAZDate}时间格式不正确!`);
+    }
+    // end 检查时间格式
+  }
+
   static async mainProcess(req, res, next, user, transaction) {
-    const { ids, AZGUserId } = req.body;
+    const { ids, YJAZDate } = req.body;
 
     // 检查相关记录是否属于用户操作范围, 记录状态是否是可操作状态
 
@@ -29,49 +40,43 @@ export default class SetWLBHs0AZG extends BusinessApiBase {
     }
     // end 检查WLBHIds存在
 
-    // 检查WLBH是否属于同一个YJZXTime
-    const tmpWLBHYJZXTimes = tmpWLBHs.map(item => item.YJZXTime);
-    const tmpUniqueWLBHYJZXTimes = [...new Set(tmpWLBHYJZXTimes)];
-
-    if (tmpUniqueWLBHYJZXTimes.length !== 1) {
-      throw new Error('物料补货应该要存在并属于同一个预计装箱时间!');
-    }
-    // end 检查WLBH是否属于同一个YJZXTime
-
-    const tmpYJZXTime = tmpUniqueWLBHYJZXTimes[0];
-
     // 检查WLBH是否属于同一个AZGS
     const tmpWLBHAZGSIds = tmpWLBHs.map(item => item.AZGSId);
     const tmpUniqueWLBHAZGSIds = [...new Set(tmpWLBHAZGSIds)];
 
     if (tmpUniqueWLBHAZGSIds.length !== 1) {
-      throw new Error('物料补货应该要存在并属于同一个安装公司!');
+      throw new Error('补货物料应该要存在并属于同一个安装公司!');
     }
     // end 检查WLBH是否属于同一个AZGS
 
     const tmpAZGSId = tmpUniqueWLBHAZGSIds[0];
 
-    // 检查AZGS是当前用户所属AZGS
-    const tmpAZGS = await user.checkAZGSId(tmpAZGSId, transaction);
-    // end 检查AZGS是当前用户所属AZGS
-
     // 检查WLBH状态是'通过' >= x < '收货'之前
-    const notCSWLBHs = tmpWLBHs.filter(item =>
-      !(
+    const notSetAGZSWLBHs = [];
+    const notCSWLBHs = tmpWLBHs.filter(item => {
+      if (!item.AZGSId) {
+        notSetAGZSWLBHs.push(item);
+      }
+      return !(
         DBTables.WLBHStatusMap.get(item.status) <
-            DBTables.WLBHStatusMap.get(DBTables.WLBHStatus.SH) &&
+            DBTables.WLBHStatusMap.get(DBTables.WLBHStatus.WC) &&
           DBTables.WLBHStatusMap.get(item.status) >=
             DBTables.WLBHStatusMap.get(DBTables.WLBHStatus.TG)
-      ));
+      )
+    });
 
     if (notCSWLBHs.length > 0) {
-      throw new Error(`${notCSWLBHs}不在${DBTables.WLBHStatus.TG}状态之后并且${DBTables.WLBHStatus.SH}状态之前, 不可指定安装工!`);
+      throw new Error(`补货物料[${notCSWLBHs[0].id}]不在${DBTables.WLBHStatus.TG}状态之后并且${DBTables.WLBHStatus.SH}状态之前, 不可指定预计安装时间!`);
+    }
+
+    if (notSetAGZSWLBHs.length > 0) {
+      throw new Error(`补货物料[${notSetAGZSWLBHs[0].id}]没有设置安装公司, 不可指定预计安装时间!`);
     }
     // end 检查WLBH状态是'通过' >= x < '收货'之前
 
-    // 检查AZGUserId是否属于tmpAZGS
-    const AZGUser = await tmpAZGS.checkAZGUserId(AZGUserId, transaction);
-    // end 检查AZGUserId是否属于tmpAZGS
+    // 检查AZGS是当前用户所属AZGS
+    const tmpAZGS = await user.checkAZGSId(tmpAZGSId, transaction);
+    // end 检查AZGS是当前用户所属AZGS
 
     // end 检查相关记录是否属于用户操作范围, 记录状态是否是可操作状态
 
@@ -79,7 +84,7 @@ export default class SetWLBHs0AZG extends BusinessApiBase {
 
     await DBTables.WLBH.update(
       {
-        AZGUserId,
+        YJAZDate,
       },
       {
         where: {
