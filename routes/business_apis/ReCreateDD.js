fe3394cfd06766a0dd68a439c3ea9d09a1c5dbab
jayliang701 <156070304@qq.com>
2018-06-30 14:43:34 +0800

    update api

--- routes/business_apis/ReCreateDD.js
+++ routes/business_apis/ReCreateDD.js
@@ -1,6 +1,8 @@
 import BusinessApiBase from '../BusinessApiBase';
 import * as DBTables from '../../models/Model';
 import { PP_DDOperationLock } from './CreateDD';
+import * as ppUtils from './ppUtils';
+import moment from 'moment';
 
 export default class ReCreateDD extends BusinessApiBase {
   static getAllowAccessJSs() {
@@ -34,6 +36,33 @@ export default class ReCreateDD extends BusinessApiBase {
     const result = r[0];
     if (result.code <= 0) {
       throw Error(result.msg);
+    }
+
+    // 先删除之前创建的陈列快照
+    await DBTables.DDGTCL.destroy({
+        where: {
+            DDId
+        },
+        transaction
+    });
+
+    // 获取和此订单有关的柜台id数组
+    const GTIds = await ppUtils.getGTListInDD(DDId, transaction);
+
+    for (let GTId of GTIds) {
+      // 创建柜台陈列快照数据
+      let CL = await ppUtils.getGTCL(GTId, transaction);
+
+      const now = moment(Date.now()).format('YYYY-MM-DD hh:mm:ss');
+      await DBTables.sequelize.query(`
+        INSERT INTO DDGTCL
+        (DDId, GTId, DWCL, WLCL, createdAt, updatedAt)
+        VALUES
+        (${DDId}, ${GTId}, '${JSON.stringify(CL.DWCLs)}', '${JSON.stringify(CL.WLCLs)}', '${now}', '${now}')
+      `, { 
+        type: DBTables.sequelize.QueryTypes.INSERT,
+        transaction 
+      });
     }
     // end 重置DD和相关Snapshot
 
