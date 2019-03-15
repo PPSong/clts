524f9eb594bfdd11d0675326938677d81657b51c
jayliang701 <156070304@qq.com>
2019-03-15 16:05:24 +0800

    更新数据库连接配置

--- config/test/setting.js
+++ config/test/setting.js
@@ -7,5 +7,26 @@ module.exports = {
     },
     cors: {
         enable: false
-    }
+    },
+
+    model: {
+        rdb: {
+            host: "127.0.0.1",
+            port: 3306,
+            user: "root",
+            password: "magicFish@SH2019",
+            database: "cltp",
+            dialect: 'mysql',
+            dialectOptions: {
+                multipleStatements: true,
+            },
+            logging: false,
+            insecureAuth: true,
+            pool: {
+                max: 5,
+                idle: 30000,
+                acquire: 60000
+            }
+        }
+    },
 };
